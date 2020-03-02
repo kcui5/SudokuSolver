a89b8003b24a85bb7eb7e3c6315094a054b087ee
kcui5 <kyle.tz.cui@gmail.com>
2020-03-02 16:23:28 -0500

    Update Main.py
    Initialized elementary functions

--- Main.py
+++ Main.py
@@ -1,1 +1,29 @@
 #Sudoku Solver
+
+def setSudokus(sudoku):
+    #Set the values in each item of the sudoku
+    
+
+    return sudoku
+
+def printSudoku(sudoku):
+    #Prints the sudoku
+
+    return None
+
+def getRow(sudoku, rowNum):
+    #Returns rowNum row of the sudoku
+
+    return row
+
+def getCol(sudoku, colNum):
+    #Returns colNum column of the sudoku
+
+    return col
+
+def getGrid(sudoku, gridNum):
+    #Returns gridNum grid (3 by 3) of the sudoku
+
+    return grid
+    
+sudoku = [None]*81
