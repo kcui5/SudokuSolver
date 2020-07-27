28355afdfdac4f8461817f01f36227ce142a89ff
kcui5 <kyle.tz.cui@gmail.com>
2020-07-26 20:49:12 -0400

    Changed Sudoku Board structure
    Created new SudokuBoard class container \ Current error exists in manual input of board items

--- Main.py
+++ Main.py
@@ -1,34 +1,64 @@
-#Sudoku Solver
+#Sudoku Solver - an algorithm designed to efficiently solve a sudoku board
 
-def setSudoku(sudoku):
-    #Set the values in each item of the sudoku
-    
+class SudokuBoard:
+    def __init__(self, values: "dict"):
+        """Initialize the board by setting the values in each item of the sudoku"""
+        #Create an empty 9x9 2D array
+        self.board = [None for _ in range(9)]
+        row = [[] for _ in range(9)]
+        self.board = [row for item in self.board]
 
-    return sudoku
+        if values:
+            #If given values, populate the board with those values
+            pass
+        else:
+            #If not given values, ask for manual input or random generation
+            validInput = False
+            while not validInput:
+                mORr = input("Enter 'M' for manual input or 'R' for a random board: ")
+                if mORr == "M" or mORr == "R":
+                    validInput = True
 
-def printSudoku(sudoku):
-    #Prints the sudoku
+        if mORr == "M":
+            for r in range(9):
+                for c in range(9):
+                    num = input(f"Row {r + 1}, Column {c + 1}: ")
+                    if num:
+                        self.board[r][c].append(int(num))
+                        num = None
+        elif mORr == "R":
+            pass
+        else:
+            print("Board could not be populated")
 
-    return None
+        print("Initialized Board:")
+        self.printSudoku()
 
-def getRow(sudoku, rowNum):
-    #Returns rowNum row of the sudoku
-    row = sudoku[9 * rowNum: 9 * rowNum + 9]
-    return row
+    def printSudoku(self):
+        """Prints the entire sudoku"""
+        for _ in range(9):
+            print(self.board[_])
+        return None
 
-def getCol(sudoku, colNum):
-    #Returns colNum column of the sudoku
-    col = []
-    for y in range(9):
-        col.append(sudoku[9 * y + colNum])
-    return col
+    def getRow(sudoku, rowNum):
+        #Returns rowNum row of the sudoku
+        row = sudoku[9 * rowNum: 9 * rowNum + 9]
+        return row
 
-def getGrid(sudoku, gridNum):
-    #Returns gridNum grid (3 by 3) of the sudoku
-    #Next two lines are likely wrong ***
-    for y in range(3 * (gridNum), 3 * (gridNum) + 3):
-        for x in range(3 * (gridNum), 3 * (gridNum) + 3)
-        
-    return grid
-    
-sudoku = [None]*81
+    def getCol(sudoku, colNum):
+        #Returns colNum column of the sudoku
+        col = []
+        for y in range(9):
+            col.append(sudoku[9 * y + colNum])
+        return col
+
+    def getGrid(sudoku, gridNum):
+        #Returns gridNum grid (3 by 3) of the sudoku
+        #Next two lines are likely wrong ***
+        grid = []
+        for y in range(3 * (gridNum), 3 * (gridNum) + 3):
+            for x in range(3 * (gridNum), 3 * (gridNum) + 3):
+                pass
+        return grid
+
+newBoard = SudokuBoard(values = {})
--- Testing.py
+++ Testing.py
@@ -1,16 +1,6 @@
 #File used to test code
 
-col = []
-for x in range(9):
-    col.append("1")
-print(col)
-
-
-sudoku = [None] * 81
-
-sudoku[42] = 52
-print(sudoku[42])
-print(sudoku[21])
-
-if sudoku[2] == None:
-    print("Nothing's here")
+if False:
+    print("Never")
+elif True:
+    print("This still runs")
